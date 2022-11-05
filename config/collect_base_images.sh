8710a773bd2c5276132844e4556625c25717cb85
muslihk_diy <muslihk00@gmail.com>
2022-11-05 22:34:17 +0530

    all the base images collect script added

---
+++ config/collect_base_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for image in   nginx \
+                postgres:14.5-bullseye \
+                certbot/certbot \
+                mcr.microsoft.com/dotnet/sdk:6.0 \
+                mcr.microsoft.com/dotnet/aspnet:6.0 \
+                python:3.10-slim-bullseye 
+do 
+docker pull $image
+done
