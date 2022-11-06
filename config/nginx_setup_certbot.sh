34c936bf03479fc318bc0230d1d5c7a08c4cc55a
muslihk_diy <muslihk00@gmail.com>
2022-11-06 12:16:39 +0530

    all the base images collect script added

--- config/nginx_setup_certbot.sh
+++ config/nginx_setup_certbot.sh
@@ -6,4 +6,5 @@ cd nginx
 DOMAIN_LIST=("admin.onlinemagic.in", "portal.onlinemagic.in", "user.onlinemagic.in")
 bash create_nginx_ssl_certbot.sh 
 cd .. && sudo docker compose --env-file ./domain.env restart reverseproxy
+sudo cp nginx/nginx_created_cert.conf nginx/nginx.conf 
 
