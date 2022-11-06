1c4eac2bedeb54452ae8f441f2a27bc61fca7a6c
muslihk_diy <muslihk00@gmail.com>
2022-11-06 11:02:01 +0530

    all the base images collect script added

--- config/domain_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## Edit domain list here
-export CERT_MAIL=muslih@gksinfotech.in 
-DOMAIN_LIST=("admin.onlinemagic.in" "portal.onlinemagic.in" "user.onlinemagic.in") 
-DOMAINS="" && for d in ${DOMAIN_LIST[@]};do DOMAINS="${DOMAINS} -d $d";done 
-export DOMAIN=$DOMAINS 
-export MAINDOMAIN=${DOMAIN_LIST[$1]} 
-printenv
