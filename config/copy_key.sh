78a09fbd2ece14178fc62195a9df3e4d94c8d3ff
root <root@vmi1067811.contaboserver.net>
2022-11-23 07:33:19 +0100

    ignore updated

--- config/copy_key.sh
+++ config/copy_key.sh
@@ -1,4 +1,4 @@
-for f in IdentityOne OmAdmin OmAdminApi OmCompany OmCustomer
+for f in IdentityOne OmAdmin OmAdminApi OmCompany OmCustomer OmOnboard
 do
 mkdir -p ./$f/ && cp  ./Keys/git/id_rsa_git ./$f/
 done 
