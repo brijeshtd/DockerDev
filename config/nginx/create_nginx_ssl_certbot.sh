a8256fddca9c03c321955b39e95c208d30010b4c
brijeshtd <30972313+brijeshtd@users.noreply.github.com>
2022-11-24 11:27:34 +0530

    domain onboarding added

--- config/nginx/create_nginx_ssl_certbot.sh
+++ config/nginx/create_nginx_ssl_certbot.sh
@@ -1,5 +1,5 @@
 DOMAIN_LIST=("tstadmin.onlinemagic.in" "tstportal.onlinemagic.in" "tstuser.onlinemagic.in" "tstonboarding.onlinemagic.in")
-SERVER_LIST=("omadmin" "omcompany" "omcustomer" "omonboarding")
+SERVER_LIST=("omadmin" "omcompany" "omcustomer" "omonboard")
 echo "
 worker_processes 1;
 events {
