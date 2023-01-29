7399c1ee9d66c9e679acb854db73a34d6e04962b
muslihk_diy <muslihk00@gmail.com>
2023-01-29 11:29:19 +0530

    service run from entrypoint.sh

--- nginx_setup_certbot.sh
+++ nginx_setup_certbot.sh
@@ -2,12 +2,14 @@
 sudo cp ./nginx/nginx_before_cert.conf ./nginx/nginx.conf 
 sudo docker compose --env-file ./domain.env up  -d reverseproxy 
 sleep 3
-sudo docker compose --env-file ./domain.env  up  certbot && \
+sudo docker compose --env-file ./domain.env  up  certbot 
 
 # create nginx.conf after succesfull certbot ssl creation
 
-cd nginx && bash create_nginx_ssl_certbot.sh && \ 
-cd .. && sudo cp ./nginx/nginx_created_cert.conf ./nginx/nginx.conf 
+cd nginx 
+bash create_nginx_ssl_certbot.sh
+cd .. 
+sudo cp ./nginx/nginx_created_cert.conf ./nginx/nginx.conf 
 sudo docker compose --env-file ./domain.env restart reverseproxy
 
 
